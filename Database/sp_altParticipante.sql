ff9e6069c5d7cd14d6c1c183e1b83297cd8530c7
Ayslan Aragao <ayslan.aragao@outlook.com>
2018-12-11 22:24:19 -0300

    Update sp_altParticipante.sql

--- Database/sp_altParticipante.sql
+++ Database/sp_altParticipante.sql
@@ -1,1 +1,18 @@
-﻿select * from tbParticipante
+﻿USE [dbClube]
+GO
+/****** Object:  StoredProcedure [dbo].[sp_altParticipante]    Script Date: 11/12/2018 22:22:56 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+ALTER procedure [dbo].[sp_altParticipante]
+@cdParticipante int = 0,
+@nmParticipante varchar(100) = '',
+@dsApelido varchar(50) = '',
+@nrDDD int = 0,
+@nrTelefone int = 0,
+@cdPartIndicador int = 0
+as
+update tbParticipante set nmParticipante = @nmParticipante, dsApelido = @dsApelido, nrDDD = @nrDDD, nrTelefone = @nrTelefone, cdPartIndicador = @cdPartIndicador
+where
+cdParticipante = @cdParticipante
